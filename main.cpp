e474af188d6ddf641fc247cfab24b26dec8eab58
vmrepo <crash221b@gmail.com>
2022-06-05 15:32:20 -0700

    added green box and jumping

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     Character player[5];
-    Character ob;
+    Character ob[2];
     UI ui;
     sf::RenderWindow window(sf::VideoMode(1500, 1000), "Sickzu neeeenja!");
     float position = player[0].getPosx();
@@ -21,6 +21,10 @@ int main()
     float speedy = rand() % 100;
     float sizex = rand() % 100;
     float sizey = rand() % 100;
+    float speedenx = rand() % 300;
+    float speedeny = rand() % 100;
+    float sizeenx = rand() % 100;
+    float sizeeny = rand() % 100;
     while (window.isOpen())
     {
         sf::Event event;
@@ -43,6 +47,32 @@ int main()
                 }
                 position -= 30;
             }
+            if ('\x20' == event.text.unicode)
+            {
+                for (int i = 0; i <= 4; i++)
+                {
+                    window.draw(player[i].getCharacter());
+                }
+                if (!flag)
+                {
+                    flag = 1;
+                    ground = 300;
+                }
+            }
+            else
+            {
+                if (flag)
+                {
+                    if (ground < 700)
+                    {
+                        ground += .5;
+                    }
+                    if(ground == 700)
+                    {
+                        flag = 0;
+                    }
+                }
+            }
             if ('\x73' == event.text.unicode)
             {
             }
@@ -50,28 +80,24 @@ int main()
             {
                 if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
                 {
-                    float hitLimity = player[1].getPosy() + player[1].getSizey();
-                    float hitLimityy = ob.getPosy() + ob.getSizey();
-                    float hitlimitx = player[1].getPosx() + player[1].getSizex();
-                    float hitlimitxx = ob.getPosx() + ob.getSizey();
-                    if ((hitLimity <= hitLimityy && hitLimity >= ob.getPosy()) && (hitlimitx <= hitlimitxx) && (hitlimitx >= ob.getPosx()))
+                    float hitLimity = (player[1].getPosy() + player[1].getSizey()) + 10;
+                    float hitLimityy = (ob[0].getPosy() + ob[0].getSizey())+10;
+                    float hitlimitx = (player[1].getPosx() + player[1].getSizex()) + 10;
+                    float hitlimitxx = (ob[0].getPosx() + ob[0].getSizey()) + 10;
+                    if ((hitLimity >= ob[0].getPosy() && hitLimity <= hitLimityy) && (hitlimitx <= hitlimitxx) && (hitlimitx >= ob[0].getPosx()))
                     {
                         count++;
-                        flag = 1;
                     }
                 }
                 if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
                 {
-                    float hitLimity = player[4].getPosy() + player[4].getSizey();
-                    float hitLimityy = ob.getPosy() + ob.getSizey();
+                    float hitLimity = (player[4].getPosy() + player[4].getSizey());
+                    float hitLimityy = (ob[0].getPosy() + ob[0].getSizey()) + 10;
                     float hitlimitx = player[4].getPosx() + player[4].getSizex();
-                    float hitlimitxx = ob.getPosx() + ob.getSizey();
-                    std::cout << hitLimity << std::endl;
-                    std::cout << hitLimityy << std::endl;
-                    if ((hitLimity <= hitLimityy && hitLimity >= ob.getPosy()) && (hitlimitx <= hitlimitxx) && (hitlimitx >= ob.getPosx()))
+                    float hitlimitxx = (ob[0].getPosx() + ob[0].getSizey()) + 10;
+                    if ((hitLimity <= hitLimityy && hitLimity >= ob[0].getPosy()) && (hitlimitx <= hitlimitxx) && (hitlimitx >= ob[0].getPosx()))
                     {
                         count++;
-                        flag = 1;
                     }
                 }
             }
@@ -85,13 +111,24 @@ int main()
         if (speedy >= 0 && speedy <= 1000)
         {
             speedy += .2;
-            ob.setCharacter(100, 100, speedx, speedy, sf::Color::Red);
-            window.draw(ob.getCharacter());
+            ob[0].setCharacter(100, 100, speedx, speedy, sf::Color::Red);
+            window.draw(ob[0].getCharacter());
         }
         else
         {
-            speedx = rand() % 1500 + 1;
+            speedx = rand() % 1000 + 1;
             speedy = 0;
+        }
+        if (speedenx >= 0 && speedenx <= 1500)
+        {
+            speedenx += .2;
+            ob[1].setCharacter(100, 100, speedenx, speedeny, sf::Color::Green);
+            window.draw(ob[1].getCharacter());
+        }
+        else
+        {
+            speedeny = rand() % 1500 + 500;
+            speedenx= 0;
         }
         ui.textBox("ArialCE.ttf",std::to_string(count),24,sf::Color::White,50,50);
         player[0].setCharacter(50, height, position, ground, sf::Color::White);
